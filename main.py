117e157cb2578cdbe6f40d7ca542ffff337cf49b
Sakshipatade <sakshivpatade@gmail.com>
2026-09-14 15:07:05 +0530

    changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, status, Request
+from fastapi import FastAPI, status, Request, Body
 from fastapi.responses import JSONResponse
 from tweet.data import USERS, COMMENTS, POSTS
 import json
-# from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -13,7 +12,8 @@ app = FastAPI()
 async def createUser(request:Request):
     data = await request.body()
     body = json.loads(data)
-    USERS.append(body)
+    if type(body.get("username")) is str:
+        USERS.append(body)
     return JSONResponse(content={"msg" : "user created"}, status_code=status.HTTP_201_CREATED)
 
 
@@ -21,3 +21,43 @@ async def createUser(request:Request):
 @app.get("/users")
 def getUsers():
     return USERS
+
+# login
+@app.post("/login")
+def loginUser(username:str=Body(...), password:str = Body(...)):
+    for user in USERS:
+        if user.get('username') == username:
+            if user.get('password') == password:
+                return JSONResponse(content={"msg":"login successful", "token":f"T-{user.get("user_id")}"}, status_code=status.HTTP_200_OK)
+            else:
+                return JSONResponse(content="Invalid Password", status_code=400)
+    return JSONResponse(content="Username not found", status_code=404)
+
+
+
+
+
+# profile 
+@app.get("/profile")
+async def getProfile(request:Request):
+    # headers = request.headers
+    token = request.headers.get('Authorization')
+    token_type, token_id = token.split('-')
+    token_id=int(token_id)
+    for user in USERS:
+        # print(user, token_id)
+        # print(token_id == user.get("user_id"))
+        if token_id == user.get("user_id"):
+            return user
+    return JSONResponse(content="Invalid token", status_code=status.HTTP_401_UNAUTHORIZED)
+
+# Get user by his id
+@app.get('/users/{id}')
+def getUser(id:int):
+    for user in USERS:
+        if user.get("user_id") == id:
+            return user
+
+    return JSONResponse(content = "user not found", status_code = 404)
+
+
